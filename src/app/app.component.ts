9a4ecde5adc836f8568114d4d67df36d4047e1b7
AllanC12 <allandevfront@gmail.com>
2025-03-10 22:21:50 -0300

    init - initial upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormLoginComponent } from './components/form-login/form-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+
+const AppRoutes: Routes = [
+  {
+    path: '',
+    component: FormLoginComponent
+  }
+]
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
