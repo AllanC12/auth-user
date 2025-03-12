4d1be5df399966180a9fb2ae167ecaa1e3d533e1
AllanC12 <allandevfront@gmail.com>
2025-03-11 22:40:52 -0300

    feat - definindo bg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 
 import { routes } from './app.routes';
-import { FormLoginComponent } from './components/form-login/form-login.component';
 
-const AppRoutes: Routes = [
-  {
-    path: '',
-    component: FormLoginComponent
-  }
-]
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FormLoginComponent } from './components/form-login/form-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  {
+    path: '',
+    component: FormLoginComponent
+  }];
