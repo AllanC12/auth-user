b0d18ccbcbf3142bff3f65a336bb9ec69428771f
AllanC12 <allandevfront@gmail.com>
2025-11-12 21:56:33 -0300

    feat - configurando readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormLoginComponent } from './components/form-login/form-login.component';
+import { FormLoginComponent } from './pages/form-login/form-login.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { FormLoginComponent } from './components/form-login/form-login.component';
-import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { FormLoginComponent } from './pages/form-login/form-login.component';
+import { FormRegisterComponent } from './pages/register/form-register/form-register.component';
 
 export const routes: Routes = [ 
   {path: '', component: FormLoginComponent},
